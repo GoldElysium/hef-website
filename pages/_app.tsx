cbdde6c9baba7f5cf284926448f99ab26ab82db9
GoldElysium <goldelysium@gmail.com>
2021-05-01 19:44:58 +0200

    Reduce DB calls

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,9 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 import 'tailwindcss/tailwind.css';
 import { AppProps } from 'next/app';
-import { Provider } from 'next-auth/client';
 
 function MyApp({ Component, pageProps }: AppProps) {
-	return (
-		<Provider session={pageProps.session}>
-			<Component {...pageProps} />
-		</Provider>
-	);
+	return <Component {...pageProps} />;
 }
 
 export default MyApp;
