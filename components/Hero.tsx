43c8c0e5b819bf79cbdf64f8a841f5b1a66ccdb7
GoldElysium <goldelysium@gmail.com>
2021-05-01 17:58:55 +0200

    Add url

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -7,7 +7,7 @@ export default function Hero() {
 					<p className="text-red-100">A place to chat with other fans of Hololive English! If you want to hang out, you've come to the right place.</p>
 				</div>
 				<div className="rounded-3xl bg-white text-red-500 text-xl font-bold w-20 h-10 flex items-center justify-center mt-4 content-end hover:text-red-700">
-					<a href="#">Join!</a>
+					<a href="https://discord.gg/holoenfans" target="_blank" rel="noreferrer">Join!</a>
 				</div>
 			</div>
 		</div>
