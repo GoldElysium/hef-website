5a24c6e774a7549258593e9ff529e21ab3a1dcba
GoldElysium <goldelysium@gmail.com>
2021-05-02 11:48:32 +0200

    Fix(Header): styling

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,10 +5,10 @@ interface IProps {
 
 export default function Header({ title, description }: IProps) {
 	return (
-		<div className="bg-red-500 w-full text-white px-4 py-2 flex min-h-52 pb-4 justify-center items-center text-center">
+		<div className="bg-red-500 w-full text-white px-4 py-2 flex pb-4 justify-center items-center text-center">
 			<div className="max-w-4xl flex flex-col">
 				<h1 className="text-5xl text-bold">{title}</h1>
-				<p className="text-red-100 text-2xl mt-4">{description}</p>
+				<p className="text-red-100 text-2xl my-4">{description}</p>
 			</div>
 		</div>
 	);
