673533716350668eee0951b68bac8340ac1933d1
GoldElysium <goldelysium@gmail.com>
2021-05-23 13:58:40 +0200

    fix(Project): check for undefined

--- pages/projects/[id].tsx
+++ pages/projects/[id].tsx
@@ -175,7 +175,7 @@ export default function ProjectPage() {
 								<div className="markdown-body">
 									<ReactMarkdown className="px-4 sm:px-0 text-black dark:text-white dark:text-opacity-80">
 										{
-											doc.description
+											doc.description && doc.description
 												.replace(/(\\\n```)/gim, '\n```')
 												.replace(/(```\n\\)|(```\n\n\\)/gim, '```\n')
 										}
