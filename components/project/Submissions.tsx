80ab68c4baf6f671366997c00ac923eea3043ffe
Julia Johannesen <julia@insertdomain.name>
2022-07-31 11:43:50 -0400

    fix: this better be the last last-minute css change

--- components/project/Submissions.tsx
+++ components/project/Submissions.tsx
@@ -118,17 +118,17 @@ const Submissions = ({ submissions, project }: IProps) => {
 							</div>
 						) || (
 							<div className="w-full h-full flex flex-wrap justify-center content-center">
-								{/*<div className="sm:w-11/12 md:w-10/12 sm:h-11/12 md:h-10/12 flex">*/}
-								{shownSubmissions.map((submission, index) => (
-									<div className="min-w-80 w-1/2 h-1/2" key={submission._id as unknown as string}>
-										<Submission
-											project={project}
-											data={submission}
-											index={index}
-										/>
-									</div>
-								))}
-								{/*</div>*/}
+								<div className="sm:w-11/12 md:w-10/12 sm:h-11/12 md:h-10/12 flex flex-wrap">
+									{shownSubmissions.map((submission, index) => (
+										<div className="min-w-80 w-1/2 h-1/2" key={submission._id as unknown as string}>
+											<Submission
+												project={project}
+												data={submission}
+												index={index}
+											/>
+										</div>
+									))}
+								</div>
 							</div>
 						)}
 					</InfiniteScroll>
