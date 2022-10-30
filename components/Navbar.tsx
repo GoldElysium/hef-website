191dd40ceff98bbd7c1e57f281a1ee7838a43559
GoldElysium <goldelysium@gmail.com>
2022-10-30 10:20:20 +0100

    fix: apply next.js 13 link codemod

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -21,12 +21,12 @@ export default function Navbar({ disableHead }: IProps) {
 				{!disableHead && <Head />}
 
 				{/* <div>
-				<Link href="/">
-					<a>
-						<img className="h-20 hidden" src="/img/logo_banner.png" alt="Logo" />
-					</a>
-				</Link>
-			</div> */}
+            <Link href="/">
+                <a>
+                    <img className="h-20 hidden" src="/img/logo_banner.png" alt="Logo" />
+                </a>
+            </Link>
+        </div> */}
 
 				<div>
 					<div className="sm:hidden">
@@ -47,20 +47,22 @@ export default function Navbar({ disableHead }: IProps) {
 								<Menu.Items static className="absolute left-2 top-16 w-32 origin-top-right bg-white divide-y divide-gray-100 rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none mr-2">
 									<div className="px-2 py-1">
 										<Menu.Item>
-											<Link href="/">
-												<a type="button" className="flex rounded-md items-center w-full">
-													<HomeIcon className="h-6 w-6 text-skin-primary-1 mr-2" />
-													Home
-												</a>
+											<Link href="/" type="button" className="flex rounded-md items-center w-full">
+
+												<HomeIcon className="h-6 w-6 text-skin-primary-1 mr-2" />
+												Home
 											</Link>
 										</Menu.Item>
 
 										<Menu.Item>
-											<Link href="/projects">
-												<a type="button" className="flex rounded-md items-center w-full mt-1">
-													<ListBulletIcon className="h-6 w-6 text-skin-primary-1 mr-2" />
-													Projects
-												</a>
+											<Link
+												href="/projects"
+												type="button"
+												className="flex rounded-md items-center w-full mt-1"
+											>
+
+												<ListBulletIcon className="h-6 w-6 text-skin-primary-1 mr-2" />
+												Projects
 											</Link>
 										</Menu.Item>
 									</div>
@@ -70,11 +72,14 @@ export default function Navbar({ disableHead }: IProps) {
 					</div>
 
 					<div className="hidden sm:block flex items-center text-lg space-x-4">
-						<Link href="/">
-							<a className="text-white font-semibold hover:text-opacity-80">Home</a>
+						<Link href="/" className="text-white font-semibold hover:text-opacity-80">
+							Home
 						</Link>
-						<Link href="/projects">
-							<a className="text-white font-semibold hover:text-opacity-80">Projects</a>
+						<Link
+							href="/projects"
+							className="text-white font-semibold hover:text-opacity-80"
+						>
+							Projects
 						</Link>
 					</div>
 				</div>
