5e4120faf5ed578a9173d9b67881b100b4fb85fc
GoldElysium <goldelysium@gmail.com>
2022-12-15 10:38:28 +0100

    fix: add missing meta tags

--- ui/Head.tsx
+++ ui/Head.tsx
@@ -22,6 +22,8 @@ export default function Head({
 	return (
 		<>
 			<title>{title ?? defaultProps.title}</title>
+			<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+			<meta charset="UTF-8" />
 
 			<meta name="keywords" content={[...(keywords ?? []), 'hololive en', 'hef', 'hololive fan', 'hololive en fan'].join(', ')} />
 			<meta name="subject" content="Hololive EN Fan Website" />
