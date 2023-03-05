9101e0a3002c4750c63233a020de57ddf4046d33
GoldElysium <goldelysium@gmail.com>
2023-03-05 19:46:54 +0100

    fix: use correct react player component

--- ui/project/experimental/sana/Submissions.tsx
+++ ui/project/experimental/sana/Submissions.tsx
@@ -94,7 +94,7 @@ function SubmissionElement({ project, submission, index }: ISubmissionProps) {
 							elements={submission.media.map((media, submissionIndex) => {
 								if (media.type === 'video') {
 									return (
-										<ReactPlayerWrapper
+										<ReactPlayer
 											width="100%"
 											height="100%"
 											key={media.id!}
