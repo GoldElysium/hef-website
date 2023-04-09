04c701a00a35b103758e743f73676bbb3d1080c5
GoldElysium <goldelysium@gmail.com>
2023-04-09 12:26:52 +0200

    feat: cookie persistence

---
+++ src/app/sitemap.xml/route.ts
@@ -0,0 +1,74 @@
+/* eslint-disable import/prefer-default-export */
+import { Project } from 'types/payload-types';
+import PayloadResponse from 'types/PayloadResponse';
+import { languages } from '../../lib/i18n/settings';
+
+export async function GET() {
+	let projects: Project[] = [];
+	let moreProjects = true;
+	let page = 1;
+
+	async function fetchNextProjects() {
+		// Fetch next page
+		const enProjectsRes = await fetch(`${process.env.NEXT_PUBLIC_CMS_URL!}/api/projects?depth=0&limit=100&page=${page}&depth=0`, {
+			headers: {
+				'X-RateLimit-Bypass': process.env.PAYLOAD_BYPASS_RATE_LIMIT_KEY ?? '',
+			} as Record<string, string>,
+		});
+		const enBody: PayloadResponse<Project> = await enProjectsRes.json();
+
+		projects = projects.concat(enBody.docs);
+
+		// Set variables for next fetch
+		page += 1;
+		moreProjects = enBody.hasNextPage;
+	}
+
+	while (moreProjects) {
+		// eslint-disable-next-line no-await-in-loop
+		await fetchNextProjects();
+	}
+
+	const xml = `\
+<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+		xmlns:xhtml="http://www.w3.org/1999/xhtml">
+	<url>
+		<loc>https://holoen.fans</loc>
+${languages.map((language) => `\
+		<xhtml:link
+			rel="alternate"
+			hreflang="${language}"
+			href="https://holoen.fans/${language}"/>
+`).join('')}\
+	</url>
+	<url>
+		<loc>https://holoen.fans/projects</loc>
+${languages.map((language) => `\
+		<xhtml:link
+			rel="alternate"
+			hreflang="${language}"
+			href="https://holoen.fans/${language}/projects"/>
+`).join('')}\
+	</url>${projects.map(((project) => `
+	<url>
+		<loc>https://holoen.fans/projects/${project.slug}</loc>
+${languages.map((language) => `\
+		<xhtml:link
+			rel="alternate"
+			hreflang="${language}"
+			href="https://holoen.fans/${language}/projects/${project.slug}"/>
+`).join('')}\
+	</url>`)).join('')}
+</urlset>\
+`;
+
+	return new Response(xml, {
+		headers: {
+			'Content-Type': 'text/xml',
+		},
+	});
+}
+
+// Allow revalidation every 30 min
+export const revalidate = 30 * 60;
--- src/middleware.ts
+++ src/middleware.ts
@@ -3,31 +3,114 @@ import { match } from '@formatjs/intl-localematcher';
 import getLanguagesFromHeaders from 'lib/util/middleware';
 import { fallbackLanguage, languages } from 'lib/i18n/settings';
 
-function isValidLocale(pathName: string) {
-	return languages.some(
+function getLocaleFromPathname(pathName: string) {
+	return languages.find(
 		(locale) => pathName.startsWith(`/${locale}/`) || pathName === `/${locale}`,
 	);
 }
 
-export function middleware(request: NextRequest) {
-	const { pathname } = request.nextUrl;
-
-	if (isValidLocale(pathname)) {
-		return NextResponse.next();
+function getUnprefixedUrl(request: NextRequest) {
+	const url = new URL(request.url);
+	if (!url.pathname.endsWith('/')) {
+		url.pathname += '/';
 	}
 
-	const acceptedLanguages = getLanguagesFromHeaders(request.headers);
-	const matchedLocale = match(acceptedLanguages, languages, fallbackLanguage);
-
-	return NextResponse.redirect(
-		new URL(`/${matchedLocale}/${pathname}`, request.url),
+	url.pathname = url.pathname.replace(
+		new RegExp(`^/(${languages.join('|')})/`),
+		'/',
 	);
+
+	// Remove trailing slash
+	if (url.pathname !== '/') {
+		url.pathname = url.pathname.slice(0, -1);
+	}
+
+	return url.toString();
+}
+
+function getAlternateEntry(url: string, locale: string) {
+	return `<${url}>; rel="alternate"; hreflang="${locale}"`;
+}
+
+// https://developers.google.com/search/docs/specialty/international/localized-versions
+function getAlternateLinksHeaderValue(request: NextRequest) {
+	const unprefixedUrl = getUnprefixedUrl(request);
+
+	const links = languages.map((language) => {
+		const url = new URL(unprefixedUrl);
+		if (url.pathname === '/') {
+			// eslint-disable-next-line no-param-reassign
+			url.pathname = `/${language}`;
+		} else {
+			// eslint-disable-next-line no-param-reassign
+			url.pathname = `/${language}${url.pathname}`;
+		}
+
+		return getAlternateEntry(url.toString(), language);
+	});
+
+	const url = new URL(unprefixedUrl);
+	url.pathname = '/en';
+	links.push(getAlternateEntry(url.toString(), 'x-default'));
+
+	return links.join(', ');
+}
+
+export function middleware(request: NextRequest) {
+	let language;
+	let response;
+
+	const { nextUrl: { pathname }, cookies } = request;
+
+	// First use route prefix
+	if (pathname) {
+		const pathLanguage = getLocaleFromPathname(pathname);
+		if (pathLanguage) {
+			response = NextResponse.next();
+			language = pathLanguage;
+		}
+	}
+
+	// If no prefix, try to find existing cookie
+	if (!language && request.cookies) {
+		if (cookies.has('NEXT_LOCALE')) {
+			const value = cookies.get('NEXT_LOCALE')?.value;
+			if (value && (languages as string[]).includes(value)) {
+				language = value;
+			}
+		}
+	}
+
+	// Lastly try to use accept language header, otherwise fall back
+	if (!language) {
+		const acceptedLanguages = getLanguagesFromHeaders(request.headers);
+		language = match(acceptedLanguages, languages, fallbackLanguage);
+	}
+
+	const hasOutdatedCookie = request.cookies.get('NEXT_LOCALE')?.value !== language;
+
+	if (!response) {
+		response = NextResponse.redirect(
+			new URL(`/${language}/${pathname}`, request.url),
+		);
+	}
+
+	if (hasOutdatedCookie) {
+		response.cookies.set('NEXT_LOCALE', language);
+	}
+
+	response.headers.set(
+		'Link',
+		getAlternateLinksHeaderValue(request),
+	);
+
+	return response;
 }
 
 export const config = {
 	matcher: [
 		// Paths to ignore
-		'/((?!api|_next/static|_next/image|favicon.ico|sw.js|resize|assets|img|locales).*)',
+		'/((?!api|_next/static|_next/image|favicon.ico|sw.js|resize|assets|img|locales|sitemap.xml|robots.txt).*)',
 
 		// Paths to check
 		'/',
