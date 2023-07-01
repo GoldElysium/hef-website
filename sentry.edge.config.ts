02ae668762e68f59cdc6c037552f273bd54e618e
GoldElysium <goldelysium@gmail.com>
2023-07-01 17:01:02 +0200

    feat: sentry monitoring

---
+++ sentry.edge.config.ts
@@ -0,0 +1,19 @@
+/* eslint-disable max-len */
+// This file configures the initialization of Sentry for edge features (middleware, edge routes, and so on).
+// The config you add here will be used whenever one of the edge features is loaded.
+// Note that this config is unrelated to the Vercel Edge Runtime and is also required when running locally.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+/* eslint-enable */
+
+import * as Sentry from '@sentry/nextjs';
+
+Sentry.init({
+	dsn: 'https://c60cc8188e7048bbb010801cd280723d@o4505454619590656.ingest.sentry.io/4505454631124992',
+
+	// Adjust this value in production, or use tracesSampler for greater control
+	tracesSampleRate: 1,
+
+	// eslint-disable-next-line max-len
+	// Setting this option to true will print useful information to the console while you're setting up Sentry.
+	debug: false,
+});
