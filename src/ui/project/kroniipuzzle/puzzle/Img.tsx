eb9f83a68d15bf285839ec09dc602f2ffe34dfdf
j1george <j1george@github.com>
2023-07-18 13:44:48 -0700

    feat: remove Img

--- src/ui/project/kroniipuzzle/puzzle/Img.tsx
+++
@@ -1,9 +0,0 @@
-/**
- * holds references to the full and thumb sized images to be associated with a puzzle piece
- */
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-class Img {
-	fullUrl: string | undefined;
-
-	thumbUrl: string | undefined;
-}
