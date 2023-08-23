ec00717fb962d9ff50510a87454736b477e72011
j1george <j1george@github.com>
2023-08-22 17:23:23 -0700

    fix: add jank padding

--- src/ui/project/kroniipuzzle/puzzle/PuzzleConfig.ts
+++ src/ui/project/kroniipuzzle/puzzle/PuzzleConfig.ts
@@ -7,8 +7,13 @@ export const SIDEBAR_WIDTH = 400;
 export const PUZZLE_WIDTH = (WORLD_WIDTH - SIDEBAR_WIDTH) / 2;
 export const PIECE_SIZE = PUZZLE_WIDTH / COL_COUNT;
 export const PIECE_MARGIN = PIECE_SIZE / 4;
+
+// todo: jank ass padding
 export const ABOUT_TEXT = `Hello there fellow Kronie!
 Thank you for your contributions to getting us to Kronii’s second anniversary, and let us hope for more fun and exciting things in the future! What you are about to come across here is a collaboration project between two fancords known as Kronii’s Clock Tower (KCT) and Kronii’s Time Vault (KTV). The project consists of an online puzzle made from scratch, with original art featuring numerous Kronii artists you may be familiar with, as well as each of your very own personalized Kronies. Your thoughtful messages will be found on the back of each puzzle piece, and can each be viewed by selecting a puzzle piece. This project was made possible by everyone from both servers, as well as participation from others like HEFS. 
 
 To our beloved Time Warden: Congratulations on your 2nd anniversary! Can you believe it? It’s been 2 years already?! You really can fly huh… But don’t worry, there’s always room for more timeless memories! I hope we haven’t puzzled you with our level of devotion… Don’t worry, there’s no missing pieces this time for you to piece together! Alright, I’ll stop. Thank you for everything, and here’s to another year!
+
+
+
 `;
