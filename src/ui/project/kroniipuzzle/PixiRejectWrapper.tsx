65fc46003df1cf716572fdd8a3c308325dda4d34
GoldElysium <goldelysium@gmail.com>
2023-08-26 13:35:32 +0200

    fix: update message

--- src/ui/project/kroniipuzzle/PixiRejectWrapper.tsx
+++ src/ui/project/kroniipuzzle/PixiRejectWrapper.tsx
@@ -18,7 +18,8 @@ export default function PixiRejectWrapper({ project, submissions }: IProps) {
 	if (OS.macOS || OS.iOS || OS.iPad) {
 		return (
 			<div className="text-center grid place-items-center text-lg font-bold min-h-screen min-w-screen h-full w-full bg-black text-white absolute">
-				Mobile Apple devices are unsupported. Please use a desktop to play this.
+				Mobile Apple devices and desktop Safari are unsupported.
+				Please use a desktop with Chrome to play this.
 			</div>
 		);
 	}
