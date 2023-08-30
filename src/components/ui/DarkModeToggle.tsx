9b1b7451d33a49a0fd7b1f488bd61d1843638fc4
GoldElysium <goldelysium@gmail.com>
2023-08-30 15:56:21 +0200

    feat: improved desktop navbar

---
+++ src/components/ui/DarkModeToggle.tsx
@@ -0,0 +1,52 @@
+'use client';
+
+import { useTheme } from 'next-themes';
+import * as Switch from '@radix-ui/react-switch';
+import { useEffect, useState } from 'react';
+
+export default function DarkModeToggle() {
+	const { resolvedTheme, setTheme } = useTheme();
+	const [mounted, setMounted] = useState(false);
+
+	useEffect(() => {
+		setMounted(true);
+	}, []);
+
+	if (!mounted) {
+		return (
+			<div className="ml-6 flex flex-nowrap items-center align-middle">
+				<Switch.Root
+					checked={false}
+					className="h-10 w-[4.25rem] rounded-full bg-black/40 px-2"
+					disabled
+				>
+					<Switch.Thumb className="block h-6 w-6 rounded-full bg-white/80 transition-transform duration-150 data-[state=checked]:translate-x-[1.75rem] motion-reduce:transition-none" />
+				</Switch.Root>
+				<img
+					src="/img/nightMode.svg"
+					alt="Night Mode Icon"
+					className="ml-2 inline h-auto w-8"
+				/>
+			</div>
+		);
+	}
+
+	return (
+		<div className="ml-6 flex flex-nowrap items-center align-middle">
+			<Switch.Root
+				checked={resolvedTheme === 'dark'}
+				onCheckedChange={() => {
+					setTheme(resolvedTheme === 'dark' ? 'light' : 'dark');
+				}}
+				className="h-10 w-[4.25rem] rounded-full bg-black/40 px-2"
+			>
+				<Switch.Thumb className="block h-6 w-6 rounded-full bg-white/80 transition-transform duration-150 data-[state=checked]:translate-x-[1.75rem] motion-reduce:transition-none" />
+			</Switch.Root>
+			<img
+				src="/img/nightMode.svg"
+				alt="Night Mode Icon"
+				className="ml-2 inline h-auto w-8"
+			/>
+		</div>
+	);
+}
