4ea93d1978a179423709c6b2dd049c9f5af0b387
GoldElysium <goldelysium@gmail.com>
2023-08-30 17:29:50 +0200

    fix: use nav element

--- src/components/ui/MobileNav.tsx
+++ src/components/ui/MobileNav.tsx
@@ -2,10 +2,10 @@
 
 export default function MobileNav() {
 	return (
-		<div
+		<nav
 			className="max-w-screen z-2000 fixed h-full w-full overflow-y-scroll bg-skin-header/50 px-8 py-4 text-skin-header-foreground backdrop-blur-2xl dark:bg-skin-header-dark/50 dark:text-skin-header-foreground"
 		>
 			<p>Test</p>
-		</div>
+		</nav>
 	);
 }
