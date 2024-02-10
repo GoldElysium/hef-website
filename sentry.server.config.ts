8ad475a3657005d2080b7ea654411cae12180b10
GoldElysium <goldelysium@gmail.com>
2024-02-10 12:23:47 +0100

    feat: add Sentry

---
+++ sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from '@sentry/nextjs';
+
+Sentry.init({
+	dsn: 'https://c60cc8188e7048bbb010801cd280723d@o4505454619590656.ingest.sentry.io/4505454631124992',
+
+	// Adjust this value in production, or use tracesSampler for greater control
+	tracesSampleRate: 1,
+
+	// Setting this option to true will print useful information to the console while you're setting up Sentry.
+	debug: false,
+});
