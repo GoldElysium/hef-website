a0345250fd8872c94778f7b1b812efa4c8ca0284
GoldElysium <goldelysium@gmail.com>
2024-03-08 16:52:58 +0100

    feat: update marker and allow multiple submissions on same marker

--- src/components/ui/project/kroniimap/KroniiMap.tsx
+++ src/components/ui/project/kroniimap/KroniiMap.tsx
@@ -12,6 +12,7 @@ import { useMemo, useState } from 'react';
 import { XMarkIcon } from '@heroicons/react/20/solid';
 import SubmissionGallery from '@/components/ui/project/SubmissionGallery';
 import Image from 'next/image';
+import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/24/outline';
 
 export interface MarkerMap {
 	[key: string]: {
@@ -38,7 +39,14 @@ function MapMarker({ coordinates, onClick }: IMapMarkerProps) {
 
 	return (
 		<Marker coordinates={coordinates} onClick={onClick}>
-			<circle r={1.5 / Math.max(panZoom.k as number / 2, 1)} fill="#F53" className="cursor-pointer" />
+			<image
+				href="https://cdn.holoen.fans/hefw/assets/kroniimap/kronii-wave.gif"
+				height={5 / Math.max(panZoom.k as number / 6, 1)}
+				width={5 / Math.max(panZoom.k as number / 6, 1)}
+				x={-2.5 / Math.max(panZoom.k as number / 6, 1)}
+				y={-2.5 / Math.max(panZoom.k as number / 6, 1)}
+				className="cursor-pointer"
+			/>
 		</Marker>
 	);
 }
@@ -141,15 +149,53 @@ export default function KroniiMap({ submissions, markerMap }: IProps) {
 					(
 						<div className="fixed left-0 top-0 z-20 grid min-h-screen w-full place-items-center bg-black/40 text-white">
 							<div className="relative">
-								<div className="absolute -top-16 flex gap-8">
+								<div className="absolute -top-16 flex items-center gap-8">
 									<button
 										type="button"
 										onClick={() => setOpenSubmissions([])}
-										className="flex items-center gap-2 rounded-lg bg-[#0869EC] px-4 py-2 text-lg text-white dark:bg-[#216FD9]"
+										className="flex items-center gap-2 rounded-lg bg-skin-primary px-4 py-2 text-lg text-white dark:bg-skin-primary-dark"
 									>
 										<XMarkIcon className="size-6" />
 										Close
 									</button>
+
+									{openSubmissions.length > 1 && (
+										<div
+											className="flex items-center justify-center rounded-md bg-skin-secondary px-2 py-1 text-center font-bold"
+										>
+											<ChevronLeftIcon
+												className={
+													submissionIndex > 0
+														? 'dark:text-skin-text-dark-dark size-8 cursor-pointer text-skin-text'
+														: 'dark:text-skin-dark-primary size-8 text-skin-primary text-opacity-30 dark:text-opacity-30'
+												}
+												onClick={() => {
+													if (submissionIndex > 0) {
+														setSubmissionIndex(submissionIndex - 1);
+													}
+												}}
+											/>
+											<span className="text-black dark:text-white">
+												{submissionIndex + 1}
+												/
+												{openSubmissions.length}
+											</span>
+											<ChevronRightIcon
+												className={
+													submissionIndex + 1 < openSubmissions.length
+														? 'dark:text-skin-text-dark-dark size-8 cursor-pointer text-skin-text'
+														: 'dark:text-skin-dark-primary size-8 text-skin-primary text-opacity-30 dark:text-opacity-30'
+												}
+												onClick={() => {
+													if (
+														submissionIndex + 1 < openSubmissions.length
+													) {
+														setSubmissionIndex(submissionIndex + 1);
+													}
+												}}
+											/>
+										</div>
+									)}
 								</div>
 								<div className="flex items-center gap-4">
 									<div className="h-[36rem] w-[24rem] overflow-y-auto bg-white px-4 py-8 text-black">
@@ -159,9 +205,9 @@ export default function KroniiMap({ submissions, markerMap }: IProps) {
 													<img
 														src={
 															hasSignature
-															// eslint-disable-next-line max-len
+																// eslint-disable-next-line max-len
 																? currentSubmission.media![currentSubmission.media!.length - 2].image.url!
-															// eslint-disable-next-line max-len
+																// eslint-disable-next-line max-len
 																: currentSubmission.media![currentSubmission.media!.length - 1].image.url!
 														}
 														alt="Kronie"
