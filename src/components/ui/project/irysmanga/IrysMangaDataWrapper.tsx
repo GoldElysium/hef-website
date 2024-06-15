036667b9357e0fe7926a314a8388e0cdcdde83a6
GoldElysium <goldelysium@gmail.com>
2024-06-15 11:04:03 +0200

    fix: force client side rendering

--- src/components/ui/project/irysmanga/IrysMangaDataWrapper.tsx
+++ src/components/ui/project/irysmanga/IrysMangaDataWrapper.tsx
@@ -1,8 +1,8 @@
 import { Project } from '@/types/payload-types';
 import { Language } from '@/lib/i18n/languages';
 import { Jura } from 'next/font/google';
+import dynamic from 'next/dynamic';
 import { MangaProvider } from './context/MangaContext';
-import IrysManga from './IrysManga';
 import getManga from './utils/data-helper';
 import { getImageUrl } from '../../old/Image';
 
@@ -56,6 +56,8 @@ export default async function IrysMangaDataWrapper({
 	lang,
 }: IProps) {
 	const optimizedImages = await fetchOptimizedImageURLs({ project, lang });
+	const IrysManga = dynamic(() => import('./IrysManga'), { ssr: false });
+
 	return (
 		<div className={jura.className}>
 			<MangaProvider
