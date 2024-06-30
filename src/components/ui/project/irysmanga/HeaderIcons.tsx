896cc2b95adf39b0fa543f254bd54f613fd58107
LegendAVRS <61302586+LegendAVRS@users.noreply.github.com>
2024-06-30 11:52:18 +0700

    chore: small fix on header icon so that the console would shut up

--- src/components/ui/project/irysmanga/HeaderIcons.tsx
+++ src/components/ui/project/irysmanga/HeaderIcons.tsx
@@ -30,14 +30,14 @@ export function HideHeaderIcon() {
 			xmlns="http://www.w3.org/2000/svg"
 			fill="none"
 			viewBox="0 0 24 24"
-			stroke-width="1.5"
+			strokeWidth="1.5"
 			stroke="currentColor"
 			className="setting-icon size-6"
 		>
 			<path
-				stroke-linecap="round"
-				stroke-linejoin="round"
-				d="M3 8.25V18a2.25 2.25 0 0 0 2.25 2.25h13.5A2.25 2.25 0 0 0 21 18V8.25m-18 0V6a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 6v2.25m-18"
+				strokeLinecap="round"
+				strokeLinejoin="round"
+				d="M3 8.25V18a2.25 2.25 0 002.25 2.25h13.5A2.25 2.25 0 0021 18V8.25M3 8.25V6a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 6v2.25"
 			/>
 		</svg>
 	);
@@ -49,14 +49,14 @@ export function ShowHeaderIcon() {
 			xmlns="http://www.w3.org/2000/svg"
 			fill="none"
 			viewBox="0 0 24 24"
-			stroke-width="1.5"
+			strokeWidth="1.5"
 			stroke="currentColor"
 			className="setting-icon size-6"
 		>
 			<path
-				stroke-linecap="round"
-				stroke-linejoin="round"
-				d="M3 8.25V18a2.25 2.25 0 0 0 2.25 2.25h13.5A2.25 2.25 0 0 0 21 18V8.25m-18 0V6a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 6v2.25m-18 0h18M5.25"
+				strokeLinecap="round"
+				strokeLinejoin="round"
+				d="M3 8.25V18a2.25 2.25 0 002.25 2.25h13.5A2.25 2.25 0 0021 18V8.25M3 8.25V6a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 6v2.25M3 8.25h18"
 			/>
 		</svg>
 	);
