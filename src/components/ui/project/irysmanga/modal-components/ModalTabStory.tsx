97b76d921c3fc287f25babe4f96a2c31d4b8afdd
LegendAVRS <61302586+LegendAVRS@users.noreply.github.com>
2024-07-09 13:53:16 +0700

    chore: added loading icon while waiting for backroom image

--- src/components/ui/project/irysmanga/modal-components/ModalTabStory.tsx
+++ src/components/ui/project/irysmanga/modal-components/ModalTabStory.tsx
@@ -1,23 +1,35 @@
-import Image from 'next/image';
+import NextImage from 'next/image';
+import { useState } from 'react';
 import ModalTabContent from './ModalTabContent';
 import { useMangaContext } from '../context/MangaContext';
 import { getLocalisedModalData, getModalDataRoot } from '../utils/types';
+import LoadingIcon from '../LoadingIcon';
 
 export default function ModalTabStory() {
 	const { manga, readerLanguage } = useMangaContext();
-	const modalData = getLocalisedModalData(getModalDataRoot(manga), readerLanguage);
+	const modalData = getLocalisedModalData(
+		getModalDataRoot(manga),
+		readerLanguage,
+	);
+	const [loading, setLoading] = useState(true);
 
 	return (
 		<ModalTabContent>
-			<h1 className="mb-4 text-4xl font-bold">{modalData.storyGreeting}</h1>
+			<h1 className="mb-4 text-4xl font-bold">
+				{modalData.storyGreeting}
+			</h1>
 			<p className="mb-4 whitespace-pre-line">{modalData.storyContent}</p>
-			<figure>
-				<Image
+
+			<figure className="relative">
+				{loading && <LoadingIcon />}
+
+				<NextImage
 					src={modalData.storyImage}
 					alt="backroom image"
 					width={1919}
 					height={1079}
 					className="rounded-lg"
+					onLoad={() => setLoading(false)}
 				/>
 				<figcaption className="mt-2 text-center text-sm">
 					{modalData.storyImageCaption}
